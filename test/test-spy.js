dc24258501411de554f1b6d074ce12fcf7e1d103
Jesse Clark <contact@jessclark.com>
2016-03-19 12:35:10 -0600

    Use before to setup spies. then also test out what beforeEach() does.

--- test/test-spy.js
+++ test/test-spy.js
@@ -6,32 +6,63 @@ var assert = require('chai').assert,
 
 
 describe("Spy()",function(){
-    
+    var callbackOnce;
+    var callbackTwice;
+    var callbackThrice;
+
+    //before is called once before the tests below are executed.
+    before(function() {
+      //console.log("BEFORE HIT!");
+      //setup the spies before:
+      callbackOnce = sinon.spy(app.spy, "once");
+      callbackTwice = sinon.spy(app.spy, "twice");
+      callbackThrice = sinon.spy(app.spy, "thrice");
+      callbackCallAll = sinon.spy(app.spy, "callAll");
+    });
+
+    //beforeEach is called each time before the tests below
+    beforeEach(function(){
+      //console.log("BEFORE EACH!");
+    })
+
     it('function to be spied on can be accessed', function(){
         assert.equal(1, app.spy.once());
     })
     
     it('calls the function', function(){
        
-       var callback = sinon.spy(app.spy, "once");
+       
        app.spy.once();       
-       assert.isTrue(callback.called);
+       assert.isTrue(callbackOnce.called);
                
     });
     
     it('does not call function', function(){
        
-       var callback = sinon.spy(app.spy, "twice");
+       
        app.spy.once();       
-       assert.isFalse(callback.called);
+       assert.isFalse(callbackTwice.called);
 
     });
     
     it('calls function from other function', function(){
        
-       var callback = sinon.spy(app.spy, "thrice");
+       
        app.spy.callAll();
-       assert.isTrue(callback.called);
+       assert.isTrue(callbackThrice.called);
+       
+    });
+
+    it('count the number of callbacks', function(){
+
+      assert.equal(callbackThrice.callCount, 1);
+      app.spy.thrice();
+
+      assert.equal(callbackOnce.callCount, 4);
+      assert.equal(callbackTwice.callCount, 1);
+      assert.equal(callbackThrice.callCount, 2);
+
+      assert.equal(callbackCallAll.callCount, 1);
 
     });
     
